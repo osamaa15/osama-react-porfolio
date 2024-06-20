350fa1ecbe8c496349037c7862d2e8c50cec05ca
osamaa15 <osaift@gmail.com>
2024-06-20 10:47:00 +0500

    delete api

--- src/pages/CommentList.tsx
+++ src/pages/CommentList.tsx
@@ -5,13 +5,15 @@ import {
   Flex,
   Group,
   LoadingOverlay,
+  Modal,
   Stack,
   Table,
   Text,
   Title,
 } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { notifications } from "@mantine/notifications";
-import { IconChevronLeft, IconRefresh } from "@tabler/icons-react";
+import { IconChevronLeft, IconRefresh, IconTrash } from "@tabler/icons-react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -21,15 +23,19 @@ function CommentList() {
   const [comments, setComments] = useState<any>([]);
   const [processing, setProcessing] = useState<boolean>(false);
   const [overlayProcessing, setOverlayProcessing] = useState<boolean>(false);
+  const [userCommentsRecord, setUserCommentsRecord] = useState<any>({});
+
+  const [opened, { open, close }] = useDisclosure(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    // setOverlayProcessing(true);
+    setOverlayProcessing(true);
     axios
       .get("https://comment-system-be.vercel.app/api/comments")
       .then((res) => {
         setComments(res?.data);
         setOverlayProcessing(false);
+        console.log(res?.data);
       })
       .catch((err) => {
         setOverlayProcessing(false);
@@ -43,8 +49,11 @@ function CommentList() {
           autoClose: 3000,
           withCloseButton: true,
         });
+      })
+      .finally(() => {
+        setOverlayProcessing(false);
       });
-  }, [comments]);
+  }, []);
 
   //   Handle Functions
 
@@ -53,24 +62,33 @@ function CommentList() {
       <Table.Td>{comment.name}</Table.Td>
       <Table.Td>{comment.email}</Table.Td>
       <Table.Td>{comment.comment}</Table.Td>
+      <Table.Td>
+        <ActionIcon
+          onClick={() => {
+            setUserCommentsRecord(comment ?? null);
+            open();
+          }}
+        >
+          <IconTrash />
+        </ActionIcon>
+      </Table.Td>
     </Table.Tr>
   ));
 
-  function handleRefreshCommentsRecords() {
-    setProcessing(true);
+  function handleDeleteCommentRecord(_id: string) {
     axios
-      .get("https://comment-system-be.vercel.app/api/comments")
+      .delete(`https://comment-system-be.vercel.app/api/comments/${_id}`)
       .then((res) => {
-        setComments(res?.data);
         notifications.show({
           title: "Hurray!",
-          message: "Refreshed Sucessfully.",
+          message: "Record Deleted Successfully.",
           radius: "md",
           withBorder: true,
           autoClose: 3000,
           withCloseButton: true,
         });
-        setProcessing(false);
+        handleRefreshCommentsRecords(false);
+        console.log(res?.data);
       })
       .catch((err) => {
         notifications.show({
@@ -83,6 +101,42 @@ function CommentList() {
           autoClose: 3000,
           withCloseButton: true,
         });
+        handleRefreshCommentsRecords(false);
+        console.log(err?.data);
+      });
+  }
+
+  function handleRefreshCommentsRecords(isShowRefreshAlert: boolean) {
+    setProcessing(true);
+    axios
+      .get("https://comment-system-be.vercel.app/api/comments")
+      .then((res) => {
+        setComments(res?.data);
+
+        isShowRefreshAlert &&
+          notifications.show({
+            title: "Hurray!",
+            message: "Refreshed Sucessfully.",
+            radius: "md",
+            withBorder: true,
+            autoClose: 3000,
+            withCloseButton: true,
+          });
+
+        setProcessing(false);
+      })
+      .catch((err) => {
+        isShowRefreshAlert &&
+          notifications.show({
+            title: "Oh no!",
+            message:
+              err.message ?? "Something went wrong, please try again later",
+            radius: "md",
+            color: "red",
+            withBorder: true,
+            autoClose: 3000,
+            withCloseButton: true,
+          });
         setProcessing(false);
       });
   }
@@ -91,7 +145,7 @@ function CommentList() {
   return (
     <Container>
       <Flex align={"center"} h={"100vh"}>
-        <Stack>
+        <Stack pos={"relative"}>
           <Group justify="center" align="center">
             <ActionIcon onClick={() => navigate("/")}>
               <IconChevronLeft />
@@ -103,6 +157,7 @@ function CommentList() {
               </Text>
             </Title>
           </Group>
+          <LoadingOverlay visible={overlayProcessing} />
           <Table.ScrollContainer
             minWidth={600}
             mah={600}
@@ -119,13 +174,13 @@ function CommentList() {
               withColumnBorders
               pos={"relative"}
             >
-              <LoadingOverlay visible={overlayProcessing} />
               <LoadingOverlay visible={processing} />
               <Table.Thead>
                 <Table.Tr>
                   <Table.Th>Name</Table.Th>
                   <Table.Th>Email</Table.Th>
                   <Table.Th>Comments</Table.Th>
+                  <Table.Th>Actions</Table.Th>
                 </Table.Tr>
               </Table.Thead>
               <Table.Tbody>{rows}</Table.Tbody>
@@ -135,7 +190,9 @@ function CommentList() {
             <Button
               leftSection={!processing && <IconRefresh />}
               variant="outline"
-              onClick={handleRefreshCommentsRecords}
+              onClick={() => {
+                handleRefreshCommentsRecords(true);
+              }}
               disabled={processing}
               loading={processing}
             >
@@ -144,6 +201,36 @@ function CommentList() {
           </Group>
         </Stack>
       </Flex>
+
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Confirm Delete?"
+        size={"lg"}
+        centered
+      >
+        <Stack>
+          <Text>This action will permanently delete the selected record!</Text>
+          <Text size="sm">
+            Selected Record: {userCommentsRecord?.name} (
+            {userCommentsRecord?.email})
+          </Text>
+          <Group justify="end">
+            <Button variant="outline" onClick={close}>
+              Cancel
+            </Button>
+            <Button
+              variant="outline"
+              onClick={() => {
+                handleDeleteCommentRecord(userCommentsRecord?.["_id"]);
+                close();
+              }}
+            >
+              Delete
+            </Button>
+          </Group>
+        </Stack>
+      </Modal>
     </Container>
   );
 }
