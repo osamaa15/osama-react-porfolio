d67a0f6acefe413718da4b61613eb4d5834bd7af
osamaa15 <osaift@gmail.com>
2025-01-20 22:23:51 +0500

    added new project

--- src/pages/projects.ts
+++ src/pages/projects.ts
@@ -18,7 +18,7 @@ const projects = [
     type: "qa",
   },
   {
-    title: "Pushstarter",
+    title: "Pushstarter - Mobile App",
     description:
       "Please click anywhere on this card to claim your reward,this is not a fraud, trust us",
     image:
@@ -45,6 +45,15 @@ const projects = [
     type: "qa",
   },
   {
+    title: "Karla",
+    description:
+      "Please click anywhere on this card to claim your reward,this is not a fraud, trust us",
+    image:
+      "https://images.unsplash.com/photo-1506143925201-0252c51780b0?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    status: "completed",
+    type: "qa",
+  },
+  {
     title: " Vibe Design",
     description:
       "Please click anywhere on this card to claim your reward,this is not a fraud, trust us",
@@ -52,6 +61,15 @@ const projects = [
       "https://images.unsplash.com/photo-1506143925201-0252c51780b0?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     status: "completed",
     type: "qa",
+  },
+  {
+    title: "Plurppl",
+    description:
+      "Please click anywhere on this card to claim your reward,this is not a fraud, trust us",
+    image:
+      "https://images.unsplash.com/photo-1533899114961-3aa0579cd5b8?q=80&w=1944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    status: "completed",
+    type: "fe",
   },
   {
     title: "Leave and Attendance Record System",
